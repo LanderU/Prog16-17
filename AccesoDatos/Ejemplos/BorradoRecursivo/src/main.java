796d9b13d15081989be3af586d487e11b84aeaa3
LanderU <lander.usategui@gmail.com>
2016-09-14 19:40:28 +0200

    Añado borrado recursivo

---
+++ AccesoDatos/Ejemplos/BorradoRecursivo/src/main.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+* @author Lander
+*/
+
+
+
+public class main {
+	
+	public static void borrar (File contenido){
+		
+		File [] borrado = contenido.listFiles();
+		
+		for (int i = 0; i < borrado.length; i++){
+			
+			if (borrado[i].isDirectory()){
+				
+				borrar(borrado[i]);
+				
+			}else{
+				
+				borrado[i].delete();
+				contenido.delete();
+				
+			}// end if		
+			
+		}// end if
+		
+	}//end borrar
+	
+	
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.print("Escriba la ruta de la carpeta que quiere borrar: ");
+		String carpeta = br.readLine();
+		
+		File contenido = new File (carpeta);
+		
+		borrar(contenido);
+		
+		System.out.println("Todo borrado!!!");
+		
+
+	}// end main
+
+}// end class
