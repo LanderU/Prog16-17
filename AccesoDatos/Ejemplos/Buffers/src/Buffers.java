5966512bfc9ba08a47b07f5ebe682dc2c800a1df
LanderU <lander.usategui@gmail.com>
2016-09-14 20:52:34 +0200

    Added Buffers project

---
+++ AccesoDatos/Ejemplos/Buffers/src/Buffers.java
@@ -0,0 +1,31 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Buffers {
+	
+	public static int TAMANO_BUFFER = 100;
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		File fichero = new File("/home/lander/prueba");
+		
+		FileReader lectura = new FileReader(fichero);
+		
+		char [] carLeidos = new char [TAMANO_BUFFER];
+		
+		lectura.read(carLeidos, 20, 20);
+		
+		for (char caracter:carLeidos){
+			
+			if((int)caracter==0)
+				caracter = '-';
+			System.out.print(caracter);
+			
+		}
+
+	}// main
+
+}// class
