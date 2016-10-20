70ee09c3654039aed7a1d35a0efb1b28c1f48bbc
LanderU <lander.usategui@gmail.com>
2016-10-20 20:08:57 +0200

    Implements hash and equals

--- AccesoDatos/DataBases/DB40Ejemplo/src/Departamento.java
+++ AccesoDatos/DataBases/DB40Ejemplo/src/Departamento.java
@@ -8,6 +8,38 @@ public class Departamento {
 	private Integer numDept;
 	private String nombre;
 	
+	// Test
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		result = prime * result + ((numDept == null) ? 0 : numDept.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Departamento other = (Departamento) obj;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		if (numDept == null) {
+			if (other.numDept != null)
+				return false;
+		} else if (!numDept.equals(other.numDept))
+			return false;
+		return true;
+	}
+
 	public Departamento(int dep, String nom){
 		this.numDept = dep;
 		this.nombre = nom;
--- AccesoDatos/DataBases/DB40Ejemplo/src/Empleado.java
+++ AccesoDatos/DataBases/DB40Ejemplo/src/Empleado.java
@@ -9,8 +9,33 @@ public class Empleado {
 	private String nomEmpleado;
 	private Integer departamento;
 	
-	// Constructores
+	// Test
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((idEmpleado == null) ? 0 : idEmpleado.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Empleado other = (Empleado) obj;
+		if (idEmpleado == null) {
+			if (other.idEmpleado != null)
+				return false;
+		} else if (!idEmpleado.equals(other.idEmpleado))
+			return false;
+		return true;
+	}
 	
+	// Constructores
 	public Empleado(Integer id, String nom, Integer dep){
 		this.idEmpleado = id;
 		this.nomEmpleado = nom;
