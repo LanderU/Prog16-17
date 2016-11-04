d11065f9df8eb8f43dd62fe9b2091ff71a8123eb
LanderU <lander.usategui@gmail.com>
2016-11-04 12:14:15 +0100

    MySql11: Added first draft

---
+++ AccesoDatos/DataBases/MySql11/src/Departamento.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ * @author Lander
+ *
+ */
+public class Departamento {
+
+	private int dept_no;
+	private String dnombre;
+	private String localidad;
+	
+	public Departamento(int dep, String nom, String loc){
+		this.dept_no = dep;
+		this.dnombre = nom;
+		this.localidad = loc;
+	}// end constructor
+
+	public int getDept_no() {
+		return dept_no;
+	}
+
+	public void setDept_no(int dept_no) {
+		this.dept_no = dept_no;
+	}
+
+	public String getDnombre() {
+		return dnombre;
+	}
+
+	public void setDnombre(String dnombre) {
+		this.dnombre = dnombre;
+	}
+
+	public String getLocalidad() {
+		return localidad;
+	}
+
+	public void setLocalidad(String localidad) {
+		this.localidad = localidad;
+	}
+	
+	
+	
+	
+}// class
---
+++ AccesoDatos/DataBases/MySql11/src/MySql11.java
@@ -0,0 +1,52 @@
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MySql11 {
+	
+	public static final String FOR_NAME="com.mysql.jdbc.Driver";
+	public static final String CONNECTION="jdbc:mysql://localhost/ejercicio1?noAccessToProcedureBodies=true";
+	public static final String USERNAME="root";
+	public static final String PASSWORD="erle";
+	
+	public static void RellenarFichero() throws IOException{
+		// Datos
+		int [] numDepart = {10,20,60,70};
+		String [] nomDepart = {"CONTABILIDAD","I+D","DIRECCION","??"};
+		String [] localidad = {"SEVILLA","GIRONA","AMURRIO","TEGUCIGALPA"};
+		// Flujos
+		FileOutputStream escribir = new FileOutputStream(new File ("DatosDepartamenotos"));
+		ObjectOutputStream datos = new ObjectOutputStream(escribir);
+		Departamento d = null;
+		for (int i = 0; i < numDepart.length; i++) {
+			 d = new Departamento(numDepart[i],nomDepart[i],localidad[i]);
+			datos.writeObject(d);
+		}// end for
+		//Objeto null para controlar el bucle
+		d = null;
+		datos.writeObject(d);
+		// Cerramos los streams
+		datos.close();
+		escribir.close();
+	}// end function
+	
+	public void CompararDatos() throws ClassNotFoundException, SQLException{
+		
+		Class.forName(FOR_NAME);
+		Connection conn = DriverManager.getConnection(CONNECTION,USERNAME,PASSWORD);
+		
+	}// end function
+	
+	public static void main(String[] args) throws IOException {
+		// Rellenamos el fichero con los datos
+		RellenarFichero();
+		
+		
+		
+	}// main
+
+}// class
