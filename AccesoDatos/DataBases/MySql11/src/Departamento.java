c16cc5cbea26f0d57925f6451ed9a40f2cba1062
LanderU <lander.usategui@gmail.com>
2016-11-05 23:31:37 +0100

    Unfinished

--- AccesoDatos/DataBases/MySql11/src/Departamento.java
+++ AccesoDatos/DataBases/MySql11/src/Departamento.java
@@ -1,19 +1,59 @@
+import java.io.Serializable;
+
 /**
  * 
  * @author Lander
  *
  */
 public class Departamento implements Serializable{
-
+	
+	
 	private int dept_no;
 	private String dnombre;
 	private String localidad;
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + dept_no;
+		result = prime * result + ((dnombre == null) ? 0 : dnombre.hashCode());
+		result = prime * result + ((localidad == null) ? 0 : localidad.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Departamento other = (Departamento) obj;
+		if (dept_no != other.dept_no)
+			return false;
+		if (dnombre == null) {
+			if (other.dnombre != null)
+				return false;
+		} else if (!dnombre.equals(other.dnombre))
+			return false;
+		if (localidad == null) {
+			if (other.localidad != null)
+				return false;
+		} else if (!localidad.equals(other.localidad))
+			return false;
+		return true;
+	}
+
 	
 	public Departamento(int dep, String nom, String loc){
 		this.dept_no = dep;
 		this.dnombre = nom;
 		this.localidad = loc;
 	}// end constructor
+	
+	
 
 	public int getDept_no() {
 		return dept_no;
@@ -38,8 +78,5 @@ public class Departamento implements Serializable{
 	public void setLocalidad(String localidad) {
 		this.localidad = localidad;
 	}
-	
-	
-	
 	
 }// class
--- AccesoDatos/DataBases/MySql11/src/MySql11.java
+++ AccesoDatos/DataBases/MySql11/src/MySql11.java
@@ -1,6 +1,8 @@
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -10,6 +12,8 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.JOptionPane;
+
 public class MySql11 {
 	
 	public static final String FOR_NAME="com.mysql.jdbc.Driver";
@@ -17,6 +21,7 @@ public class MySql11 {
 	public static final String USERNAME="root";
 	public static final String PASSWORD="erle";
 	public static List<Departamento> listaDepartamentos = new ArrayList<Departamento>();
+	private static ObjectInputStream datos;
 
 	
 	public static void RellenarFichero() throws IOException{
@@ -24,27 +29,30 @@ public class MySql11 {
 		int [] numDepart = {10,20,60,70};
 		String [] nomDepart = {"CONTABILIDAD","I+D","DIRECCION","??"};
 		String [] localidad = {"SEVILLA","GIRONA","AMURRIO","TEGUCIGALPA"};
-		// Flujos
-		FileOutputStream escribir = new FileOutputStream(new File ("DatosDepartamenotos"));
-		ObjectOutputStream datos = new ObjectOutputStream(escribir);
-		Departamento d = null;
-		for (int i = 0; i < numDepart.length; i++) {
-			 d = new Departamento(numDepart[i],nomDepart[i],localidad[i]);
+		File find = new File("DatosDepartamentos");
+		if(!(find.exists())){
+			FileOutputStream escribir = new FileOutputStream(new File ("DatosDepartamentos"));
+			ObjectOutputStream datos = new ObjectOutputStream(escribir);
+			Departamento d = null;
+			for (int i = 0; i < numDepart.length; i++) {
+				 d = new Departamento(numDepart[i],nomDepart[i],localidad[i]);
+				datos.writeObject(d);
+			}// end for
+			//Objeto null para controlar el bucle
+			d = null;
 			datos.writeObject(d);
-		}// end for
-		//Objeto null para controlar el bucle
-		d = null;
-		datos.writeObject(d);
-		// Cerramos los streams
-		datos.close();
-		escribir.close();
+			// Cerramos los streams
+			datos.close();
+			escribir.close();
+			JOptionPane.showMessageDialog(null, "Datos guardados");
+		}// end if
 	}// end function
 	
-	public void RellenarLista() throws ClassNotFoundException, SQLException{
+	public static void RellenarLista() throws ClassNotFoundException, SQLException{
 		Class.forName(FOR_NAME);
 		Connection conn = DriverManager.getConnection(CONNECTION,USERNAME,PASSWORD);
 		Statement checkDepartamento = conn.createStatement();
-		ResultSet departs = checkDepartamento.executeQuery("SELECT * FROM departamentos");
+		ResultSet departs = checkDepartamento.executeQuery("SELECT * FROM departamentos ORDER BY dept_no");
 		while(departs.next()){
 			Departamento dep = new Departamento(departs.getInt(1),(String)departs.getString(2),departs.getString(3));
 			listaDepartamentos.add(dep);
@@ -55,10 +63,34 @@ public class MySql11 {
 		conn.close();
 	}// end function
 	
-	public static void main(String[] args) throws IOException {
+	public static void ActualizarDatos() throws IOException, ClassNotFoundException{
+		FileInputStream leer = new FileInputStream(new File ("DatosDepartamentos"));
+		datos = new ObjectInputStream(leer);
+		Departamento depAux = (Departamento) datos.readObject();
+		int index = 0;
+		while(depAux != null){
+			if (listaDepartamentos.contains(depAux)){
+				JOptionPane.showMessageDialog(null, "Nada que actualizar");
+			}else if(listaDepartamentos.get(index).getDept_no() != depAux.getDept_no()){
+				JOptionPane.showMessageDialog(null, "La clave primaria no puede ser modificada");
+			}else if((listaDepartamentos.get(index).getDept_no() == depAux.getDept_no()) && (!listaDepartamentos.get(index).getDnombre().equals(depAux.getDnombre())) && (listaDepartamentos.get(index).getLocalidad().equals(depAux.getLocalidad()))){
+				JOptionPane.showMessageDialog(null, "Se actualizará el nombre del departamento");
+			}else if ((listaDepartamentos.get(index).getDept_no() == depAux.getDept_no()) && (listaDepartamentos.get(index).getDnombre().equals(depAux.getDnombre())) && (!listaDepartamentos.get(index).getLocalidad().equals(depAux.getLocalidad()))){
+				JOptionPane.showMessageDialog(null, "Se actualizará la localidad");
+			}
+			
+			index++;
+		}// end while
+		
+		
+	}// end function
+	
+	
+	public static void main(String[] args) throws IOException, ClassNotFoundException, SQLException {
 		// Rellenamos el fichero con los datos
 		RellenarFichero();
-		
+		RellenarLista();
+		ActualizarDatos();
 		
 	}// main
 
